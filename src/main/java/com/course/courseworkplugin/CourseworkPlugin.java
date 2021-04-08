2e4440fca3300f00667e906fc8f0bab3ddb2a777
ilya <ivgorbach@edu.hse.ru>
2021-04-08 23:21:12 +0300

    added project and base command controllers

---
+++ src/main/java/com/course/courseworkplugin/CourseworkPlugin.java
@@ -0,0 +1,17 @@
+package com.course.courseworkplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CourseworkPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
